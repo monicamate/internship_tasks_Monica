1fb897f0bca0c52813456a8ce93239dc7cb755fb
monicamate <105111389+monicamate@users.noreply.github.com>
2022-06-15 17:49:05 +0400

    Add files via upload

---
+++ LocatatorsForMambleLabs.js
@@ -0,0 +1,11 @@
+var locForOnexHomePahe = {
+
+partnership: '//a[contains(text(), "Partnership")]',
+getInTouch: '//a[contains(text(), "Get in touch")]',
+theProcess: '//a[contains(text(), "The process")]',
+qualityAssurance: '//span[contains(text(), "Quality Assurance")]',
+becameAnIntern: '//a[@rel="noreferrer"]',
+homeHeroDiv: "//div[@class='HomeHero_homeHero__be9fO']",
+containerDiv: "//div[@class='HomeHero_homeHero__be9fO']/div[@data-testid='container']",
+
+}
