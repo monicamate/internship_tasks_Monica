c2fab20d785da1b67ecb1c65c617065351d91a72
monicamate <105111389+monicamate@users.noreply.github.com>
2022-06-15 15:46:28 +0400

    Add files via upload

---
+++ locatorsForOnexHomePage.js
@@ -0,0 +1,9 @@
+const locForOnexHomePahe = {
+
+    searchbar: '#track',
+    orderTitel: "//h2[contains(text(),'Հետեվել')]]",
+    newsBlog: 'div[class="more-btn-div"]>a[class="all-btn"]',
+    personainformation:'div[class*="field-userprofile-firstname"]',
+    settings: 'div[id="userprofile-gender"] input[value="2"]',
+
+}
